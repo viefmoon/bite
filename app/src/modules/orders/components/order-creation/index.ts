51ecd20a5e3df2c879f962536db4046d9de056f6
Leonel <viefmoon@gmail.com>
2025-07-26 21:07:54 -0600

    ttt

--- app/src/modules/orders/components/order-creation/index.ts
+++
@@ -1,3 +0,0 @@
-export { CategoryGrid } from './CategoryGrid';
-export { ShiftClosedView } from './ShiftClosedView';
-export { CategoryQuickAccess } from './CategoryQuickAccess';
--- app/src/modules/pizzaCustomizations/components/index.ts
+++
@@ -1,4 +0,0 @@
-// Componentes principales usados en screens
-export { PizzaProductsTab } from './PizzaProductsTab';
-export { PizzaCustomizationsTab } from './PizzaCustomizationsTab';
-export { PizzaCustomizationFormModal } from './PizzaCustomizationFormModal';
