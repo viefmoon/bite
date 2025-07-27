2282e584164f18e5045c5df43f2970aecaea4005
Leonel <viefmoon@gmail.com>
2025-07-26 20:57:17 -0600

    dfd

--- app/src/modules/pizzaCustomizations/components/index.ts
+++ app/src/modules/pizzaCustomizations/components/index.ts
@@ -2,9 +2,3 @@
 export { PizzaProductsTab } from './PizzaProductsTab';
 export { PizzaCustomizationsTab } from './PizzaCustomizationsTab';
 export { PizzaCustomizationFormModal } from './PizzaCustomizationFormModal';
-
-// Los siguientes componentes son internos y no necesitan ser exportados
-// PizzaCustomizationCard - usado solo en PizzaCustomizationsTab
-// PizzaConfigurationModal - usado solo en PizzaProductsTab
-// AssociatePizzaToppingsModal - usado solo en PizzaProductsTab
-// PizzaCustomizationDetailModal - usado solo en PizzaCustomizationsTab
--- app/src/modules/shiftAudit/components/index.ts
+++ app/src/modules/shiftAudit/components/index.ts
@@ -1,6 +1,5 @@
 export { ShiftOrdersModal } from './ShiftOrdersModal';
 export { OrderDetailsView } from './OrderDetailsView';
-export { OrderHistoryView } from './OrderHistoryView';
 export { OrderHistoryContent } from './OrderHistoryContent';
 export { HistoryItemComponent } from './HistoryItemComponent';
 export { ShiftSalesSummaryView } from './ShiftSalesSummaryView';
